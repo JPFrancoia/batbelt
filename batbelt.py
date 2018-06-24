b19118cb2648485aa8f89b44b408ee6dec5a438c
JPFrancoia <jeanpatrick.francoia@gmail.com>
2018-06-24 17:51:09 +0100

    Better annotations, cleaning code on batbelt.py.

--- batbelt.py
+++ batbelt.py
@@ -1,97 +1,142 @@
 #!/usr/bin/python
 # -*-coding:Utf-8 -*
 
+"""
+Useful functions for miscellanous tasks
+"""
+
 import re
-import os
-import sqlite3
 import unidecode
 import datetime
 from dateutil.parser import parse
 from dateutil import relativedelta
 import numpy as np
+from typing import List, Set, Tuple, Optional
 
 
-"""Module pour fct de secours ou qui n'ont rien à voir avec les thèmes des
-autres modules"""
+def sizeof(nbr: float, unit: str = "o", rounded: int = 1) -> str:
 
+    """sizeof
 
-def sizeof(nbr, unit="o", rounded=1):
+    Args:
+        nbr (float): number of bytes
+        unit (str): what unit to use. o for octet, iB for binary bytes
+        rounded (int): nbr of decimals
 
-    """On récupère la taille en octets, et on la formate en Mo
-    ou en Go si plus grand que 1000 Mo. Si on passe l'argument 'b'
-    en paramètre, on calcule en ibytes"""
-    # http://www.fevrierdorian.com/blog/post/2011/06/26/Taille-d-un-fichier-humainement-comprehensible-en-Python
+    Returns:
+        str: a formatted string with the proper unit
+
+    http://www.fevrierdorian.com/blog/post/2011/06/26/Taille-d-un-fichier-humainement-comprehensible-en-Python
+    """
 
     if unit == "o":
-        for x in ['octets', 'ko', 'Mo', 'Go', 'To']:
+
+        if nbr < 1000.0:
+            return "{0} {1}".format(round(nbr, rounded), "octets")
+
+        for x in ["ko", "Mo", "Go", "To"]:
             if nbr < 1000.0:
                 return "{0} {1}".format(round(nbr, rounded), x)
-            nbr /= 1000.0
-    elif unit == "b":
-        for x in ['bytes', 'KiB', 'MiB', 'GiB', 'TiB']:
+            else:
+                nbr /= 1000.0
+
+    elif unit == "iB":
+
+        if nbr < 1024.0:
+            return "{0} {1}".format(round(nbr, rounded), "bytes")
+
+        for x in ["KiB", "MiB", "GiB", "TiB"]:
             if nbr < 1024.0:
                 return "{0} {1}".format(round(nbr, rounded), x)
-            nbr /= 1024.0
+            else:
+                nbr /= 1024.0
 
 
-def simpleChar(chaine):
+def simple_char(entry: str) -> str:
 
-    """Fct retournant la version minuscule et sans accent de la chaine
-    entrante"""
-    #http://www.siteduzero.com/forum-83-810635-p1-sqlite-recherche-avec-like-insensible-a-la-casse.html#r7767300
+    """simple_char
+
+    Args:
+        entry (str): string to be simplified
+
+    Returns:
+        str: the simplified string, lower case and without accents
+
+    http://www.siteduzero.com/forum-83-810635-p1-sqlite-recherche-avec-like-insensible-a-la-casse.html#r7767300
+    """
 
     # Nouvelle fct de renommage, qui prend aussi les chiffres
-    #http://stackoverflow.com/questions/5574042/string-slugification-in-python
-    chaine = unidecode.unidecode(chaine).lower()
-    return re.sub(r'\W+', ' ', chaine)
+    # http://stackoverflow.com/questions/5574042/string-slugification-in-python
+    entry = unidecode.unidecode(entry).lower()
+    return re.sub(r"\W+", " ", entry)
 
 
-def recent(date, days=1):
+def recent(date: str, days: int = 1) -> bool:
 
-    """Fct qui renvoie True si la date passée
-    en paramètre est récente. 1 jour est le
-    paramètre par défaut pour récent"""
+    """recent
+
+    Args:
+        date (str): date formatted as a string
+        days (int): numbers of days to compare to date
+
+    Returns:
+        bool: if date is older than now + days, return False. Else return True
+    """
 
     now = datetime.datetime.now()
-
     date = parse(date)
 
-    if now - relativedelta.relativedelta(days =+ days) < date:
+    if now - relativedelta.relativedelta(days=+days) < date:
         return True
     else:
         return False
 
 
-def strByteToOctet(size):
+def strByteToOctet(size: str) -> Tuple[str, int]:
 
-    """Fct pour convertir une chaîne du type '300 MiB' en bytes,
-    et pour retourner une chaîne en Mo"""
+    """strByteToOctet
+
+    Args:
+        size (str): a size in binary bytes. ex: 300 MiB
+
+    Returns:
+        Tuple[str, int]: the size in octet, human readable, and the size in octet
+    """
 
     nbr, unit = size.split(" ")
     nbr = float(nbr)
 
     if unit == "MiB":
-        nbr *= 2**20
+        nbr *= 2 ** 20
     elif unit == "GiB":
-        nbr *= 2**30
+        nbr *= 2 ** 30
 
     nbr_octets = nbr
 
-    for x in ['o','Ko','Mo','Go','To']:
+    for x in ["o", "Ko", "Mo", "Go", "To"]:
         if nbr < 1000.0:
             return "{0} {1}".format(round(nbr, 1), x), int(round(nbr_octets, 0))
         nbr /= 1000.0
 
 
-def frange(start, end=None, inc=None):
-    "A range function, that does accept float increments..."
+def frange(start: float, end: Optional[float] = None, inc: float = 1.0) -> List[float]:
 
-    if end == None:
+    """frange
+    A range function, that accepts float increments
+    if end is not provided, use start as the end
+
+    Args:
+        start (float): start of the list
+        end (Optional[float]): end of the range
+        inc (float): increment. Default to 0.1
+
+    Returns:
+        List[float]: list of the floats incremented
+    """
+
+    if end is None:
         end = start + 0.0
         start = 0.0
-
-    if inc == None:
-        inc = 1.0
 
     L = []
     while 1:
@@ -101,11 +146,13 @@ def frange(start, end=None, inc=None):
         elif inc < 0 and next <= end:
             break
         L.append(next)
-        
+
     return L
 
 
-def red_chisq(ydata, ymod, deg=3, sd=None):
+def red_chisq(
+    ydata: np.array, ymod: np.array, deg: int, sd: np.array = None
+) -> np.array:
 
     """
     http://astropython.blogspot.fr/2012/02/computing-chi-squared-and-reduced-chi.html
@@ -117,33 +164,57 @@ def red_chisq(ydata, ymod, deg=3, sd=None):
     """
 
     if sd is None:
-        chisq = np.sum((ydata - ymod)**2)
+        chisq: np.array = np.sum((ydata - ymod) ** 2)
     else:
-        chisq = np.sum(((ydata - ymod) / sd)**2)
+        chisq: np.array = np.sum(((ydata - ymod) / sd) ** 2)
 
-    nu = len(ydata) - 1 - deg
+    nu: int = len(ydata) - 1 - deg
 
     return chisq / nu
 
 
 def droite(x, a, b):
 
-    y = a*x + b 
+    y = a * x + b
 
     return y
 
 
-def strip_tags(input_str):
+def strip_tags(input_str: str) -> str:
 
-    """Simply returns the string without the html tags"""
+    """strip_tags
 
-    return re.sub('<[^>]*>', '', input_str)
+    Args:
+        input_str (str): input string, with html tags
+
+    Returns:
+        str: stripped string, no more html tags
+    """
+
+    return re.sub("<[^>]*>", "", input_str)
 
 
-def remove_dup_order(seq):
+def remove_dup_order(seq: List[str]) -> List[str]:
 
-    """Function to remove duplicates of a list while keeping the order"""
+    """remove_dup_order
 
-    seen = set()
-    seen_add = seen.add
-    return [x for x in seq if not (x in seen or seen_add(x))]
+    Args:
+        seq (List[str]): A list of elements (str) to un-duplicate
+
+    Returns:
+        List[str]: list of elements without duplicates
+    """
+
+    seen: Set[str] = set()
+
+    # Update append a sequence to a set. If an element of the sequence is duplicate,
+    # it will be added only once to the set
+    seen.update(seq)
+
+    return list(seen)
+
+
+if __name__ == "__main__":
+    # print(strByteToOctet("300 MiB"))
+    # print(frange(10, inc=0.5))
+    pass
