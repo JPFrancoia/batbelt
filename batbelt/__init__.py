6e905926b6c673a03a0e6983fb9432e4b2969496
JPFrancoia <jeanpatrick.francoia@okra.ai>
2018-11-23 13:17:07 +0100

    Adding init file for lib.

---
+++ batbelt/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# coding: utf-8
+
+from .easyxls import EasyXls
+from .log import MyLog
+import .batbelt
+
+__all__ = [
+    "EasyXls",
+    "MyLog",
+]
+
+
+if __name__ == '__main__':
+    pass
